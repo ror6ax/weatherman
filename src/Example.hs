f10d4f73a7aba6abcfc25910da4b7e6f28c82571
Gregory Reshetniak <ror6ax@gmail.com>
2018-12-10 20:43:00 +0100

    added a module

--- src/Example.hs
+++ src/Example.hs
@@ -10,6 +10,8 @@ import qualified Web.Scotty as S
 import Web.Scotty.Trans (json, jsonData)
 import Data.Monoid ((<>))
 import Data.Aeson (FromJSON, ToJSON)
+import Data.String.Interpolate
+import Module1
 
 data Place = Place { lon :: Int, lat :: Int } deriving (Show, Generic)
 
@@ -19,14 +21,15 @@ instance FromJSON Place
 app' :: S.ScottyM ()
 app' = do
   S.get "/" $ do
-    S.text "hello, this is today's weather. Use /weather endpoint!"
+    let x = Module1.function1 123
+    S.text [i|hello, this is today's weather. Use /weather endpoint!: #{x}|]
 
   S.get "/weather" $ do
     S.json $ object ["temperature" .= Number 23, "verbal description" .= String "not Hawaii"]
 
   S.post "/weather1/:place" $ do
     place <- S.param "place"
-    S.text ("hello " <> place <> "!")    
+    S.text ("hello " <> place <> "!")
 
   S.post "/weather2/:place" $ do
     place <- jsonData :: S.ActionM Place
---
+++ src/Module1.hs
@@ -0,0 +1,6 @@
+module Module1(
+  function1
+  ) where
+
+function1 :: Int -> Int
+function1 x = 100
